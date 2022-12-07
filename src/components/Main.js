61106d70bb6dd41dc15f5bf931b598cc82c9e5d6
Denis Kolosov <ftforest640@gmail.com>
2022-12-07 16:40:36 +0700

    chapter six : StarRating component create

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,16 @@
 // ./src/components/Main.js
 
 import React from "react";
-import Menu from "./Menu";
+import Menu from "./app_menu/Menu";
 import data from "../data/recipes.json"
+import StarRating from "./star_rating/StarRating";
 
 
 function Main() {
     return (
         <article>
             <Menu recipes={data} />
+            <StarRating />
         </article>
     );
 }
